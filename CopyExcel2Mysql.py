42f1736fff37a2a29afcf2089f81998eef9ce0e8
huhanghao <517072971@qq.com>
2017-08-23 15:35:00 +0800

    测试着玩啊

--- CopyExcel2Mysql.py
+++ CopyExcel2Mysql.py
@@ -18,12 +18,12 @@ def excel_table_byindex(file='pytest.xls', colnameindex=0, by_index=0):
     colnames = table.row_values(colnameindex)
     list = []
     for rownum in range(1, nrows):
-
         row = table.row_values(rownum)
         if row:
-            app = {}
+            app = []
             for i in range(len(colnames)):
-                app[i] = row[i]
+                # 读取每个数据并放入数组
+                app.append(row[i])
             list.append(app)
     return list
 
@@ -47,23 +47,22 @@ def insertData2Sql():
         # 插入数据
         value = [2, 1]
         cursor.execute('INSERT INTO Test(f1, f2) values(%s,%s)', value)
+        # 提交至数据库
+        conn.commit()
 
         # 查询多条记录
         results = cursor.fetchmany(5)
         for r in results:
             print (r)
 
-        conn.commit()
     except Exception as e:
         # 发生错误时会滚
         conn.rollback()
     finally:
-
         # 关闭游标连接
         cursor.close()
         # 关闭数据库连接
         conn.close()
-
 
 def main():
     tables = excel_table_byindex()
